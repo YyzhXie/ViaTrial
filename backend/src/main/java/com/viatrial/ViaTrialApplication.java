5f6c57eb9d04d1a0fc238577f96364839dbd72c9
MingYX <1573452838@qq.com>
2026-06-27 11:56:50 +0800

    [Project] 创建后端基础结构

---
+++ backend/src/main/java/com/viatrial/ViaTrialApplication.java
@@ -0,0 +1,12 @@
+package com.viatrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViaTrialApplication.class, args);
+    }
+}
