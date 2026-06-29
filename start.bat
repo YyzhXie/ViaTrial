d219d53e785134eebbc7ac3df0d2211c4ec671c0
MingYX <1573452838@qq.com>
2026-06-29 23:37:52 +0800

    [Release] 发布 v0.1.2

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ setlocal
 
 cd /d "%~dp0"
 
-set "JAR=backend\target\viatrial-backend-0.1.1.jar"
+set "JAR=backend\target\viatrial-backend-0.1.2.jar"
 
 if not exist "%JAR%" (
   echo Jar not found. Building ViaTrial first...
